dc6f8a00b52688cf15297f8199009623870d9b1a
dannywinrow <dannywinrow@live.co.uk>
2023-12-02 22:21:26 +0000

    Allez cuisine day 1

---
+++ 2023/allez/1.jl
@@ -0,0 +1,9 @@
+matchlast(regex::Regex,str::AbstractString) = match(Regex("(?:$(regex.pattern))(?!.*(?:$(regex.pattern)))"),str)
+pickle(x) = @something(findfirst(==(x[1]),"123456789"),findfirst(==(x),["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]))
+
+loaf = loadlines()
+
+println("Part 1: $(sum([pickle(onion.match) for onion in match.(r"\d",loaf)])*10 +
+                    sum([pickle(onion.match) for onion in matchlast.(r"\d",loaf)]))")
+println("Part 2: $(sum([pickle(onion.match) for onion in match.(r"one|two|three|four|five|six|seven|eight|nine|\d",loaf)])*10 +
+                    sum([pickle(onion.match) for onion in matchlast.(r"one|two|three|four|five|six|seven|eight|nine|\d",loaf)]))")
