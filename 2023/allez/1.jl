b131d8f8d4fde0d694a91ea06eacda98842eee25
dannywinrow <dannywinrow@live.co.uk>
2023-12-02 22:43:34 +0000

    readlines filename

--- 2023/allez/1.jl
+++ 2023/allez/1.jl
@@ -1,7 +1,7 @@
 matchlast(regex::Regex,str::AbstractString) = match(Regex("(?:$(regex.pattern))(?!.*(?:$(regex.pattern)))"),str)
 pickle(x) = @something(findfirst(==(x[1]),"123456789"),findfirst(==(x),["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]))
 
-loaf = loadlines()
+const loaf = readlines("2023/inputs/1.txt")
 
 println("Part 1: $(sum([pickle(onion.match) for onion in match.(r"\d",loaf)])*10 +
                     sum([pickle(onion.match) for onion in matchlast.(r"\d",loaf)]))")
