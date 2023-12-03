8b6c8784f70d8e3e835835d33e5765e24f2b2b26
dannywinrow <dannywinrow@live.co.uk>
2023-12-03 19:24:47 +0000

    fix for more general cases

--- 2023/src/3.jl
+++ 2023/src/3.jl
@@ -2,10 +2,20 @@ include("../../helper.jl")
 
 input = getinput()
 
-s = count(==('\n'),input)
+s = findfirst(==('\n'),input) - 1
 grid = replace(input,"\n"=>"")
 symbols = [y[1] for y in findall(r"[^.\d]",grid)]
 nums = findall(r"\d+",grid)
+
+# In case num wraps around lines (doesn't handle num taking up whole line)
+for (i,num) in enumerate(nums)
+    line1 = (num[1]-1) ÷ s + 1
+    line2 = (num[end]-1) ÷ s + 1
+    if line2 > line1
+        nums[i] = num[1]:s*line1
+        push!(nums,s*line1+1:num[end])
+    end
+end
 
 function surround(num)
     a = num[1] % s == 1 ? num[1] : num[1] - 1
@@ -14,7 +24,7 @@ function surround(num)
 end
 
 ispart(num) = !isempty(intersect(surround(num),symbols))
-getnum(num) = parse(Int,x[num])
+getnum(num) = parse(Int,grid[num])
 
 solveit() = sum(ispart.(nums) .* getnum.(nums))
 
@@ -32,7 +42,7 @@ function numssurround(gear)
     unique!(gearnums)
 end
 
-function isgear(gear)
+function gearpower(gear)
     gearnums = numssurround(gear)
     if length(gearnums) == 2
         return *(getnum.(gearnums)...)
@@ -40,7 +50,7 @@ function isgear(gear)
     0
 end
 
-solveit2() = sum(isgear.(gears))
+solveit2() = sum(gearpower.(gears))
 
 pt2 = solveit2()
 clipboard(pt2)
