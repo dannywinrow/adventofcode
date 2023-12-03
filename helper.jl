9da820d998b1befddd3b17d09ebbfe4b9e1e0f91
dannywinrow <dannywinrow@live.co.uk>
2023-12-03 14:48:27 +0000

    reorganise helper

--- helper.jl
+++ helper.jl
@@ -17,14 +17,32 @@ const templatefile = "puzzletemplate.jl"
 const logfile = "log.txt"
 const logdelim = " | "
 
-getinput() = getinput(getyearday()...)
-function getinput(year,day;type=String)
-    filepath = getfilename(year,day)
+#
+
+createfile() = createfile(year(now()),day(now()))
+function createfile(year,day)
+    filepath = getjuliafilename(year,day)
     if !isfile(filepath)
         mkpath(dirname(filepath))
-        downloadAoCinput(year,day)
+        cp(templatefile,filepath)
     end
+end
+
+getinput() = getinput(getyearday()...)
+function getinput(year,day;type=String)
+    !isfile(getfilename(year,day)) && downloadAoCinput(year,day)
     read(filepath,type)
+end
+
+function downloadAoCinput(year,day)
+    filepath = getfilename(year,day)
+    !ispath(dirname(filepath)) && mkpath(dirname(filepath))
+    io = open(filepath, "w")
+    url = getinputurl(year,day)
+    headers = ["cookie" => "session=$sessioncookie"]
+    HTTP.request("GET",url,headers,response_stream=io)
+    close(io)
+    logaction("$year-$day","downloaded")
 end
 
 loadlines(splitbyempty=false) = loadlines(getyearday()...,splitbyempty)
@@ -55,21 +73,21 @@ function parsehashgrid(lines)
     (x -> x =="#").(hcat(split.(lines,"")...))'
 end
 
+# Broken from 2023
 submitanswer(level, answer) = submitanswer(getyearday()...,level,answer)
 function submitanswer(year, day, level, answer)
     url = getsubmiturl(year,day)
     @info url, level, answer
-    headers = Dict("cookie" => "session=$sessioncookie")
-    r = HTTP.request("POST",url,headers,"level=$level&answer=$answer")
-    s = String(r.body)
-    correct = !occursin("not the right answer",s)
-    logaction("$year-$day-$level","submit $answer, $correct")
+    #headers = Dict("cookie" => "session=$sessioncookie")
+    #r = HTTP.request("POST",url,headers,body="level=$level&answer=$answer")
+    #s = String(r.body)
+    #correct = !occursin("not the right answer",s)
+    logaction("$year-$day-$level","submit $answer")
     #if occursin("<article>",s)
     #    return match(r"<article><p>(.+)</p></article>"s,s)[1]
     #else
     #    return s
     #end
-    
 end
 
 function getyearday()
@@ -81,39 +99,6 @@ function getyearday()
     year = match(r"\d\d\d\d",String(st[i].file)).match
     day = match(r"(\d+)[^\\]*.jl",String(st[i].file)).captures[1]
     year,day
-end
-
-function downloadAoCinput(year,day)
-    ###############NEED ERROR HANDLING
-    io = open(getfilename(year,day), "w")
-    url = getinputurl(year,day)
-    headers = ["cookie" => "session=$sessioncookie"]
-    HTTP.request("GET",url,headers,response_stream=io)
-    close(io)
-    logaction("$year-$day","downloaded")
-end
-
-function splitvect(a::Vector,delim)
-    inds = vcat(0,findall(==(delim),a),length(a)+1)
-    view.(Ref(a), (:).(inds[1:end-1].+1,inds[2:end].-1))
-end
-
-freqdict(str) = Dict([i => count(x->x==i,str) for i in str])
-
-createfile() = createfile(year(now()),day(now()))
-function createfile(year,day)
-    filepath = getjuliafilename(year,day)
-    if !isfile(filepath)
-        mkpath(dirname(filepath))
-        cp(templatefile,filepath)
-    end
-end
-
-neighbours(ci) = Ref(ci) .+ cartesiancube(length(ci))
-function cartesiancube(dims,i=false)
-    ret = CartesianIndices(Tuple(fill(-1:1,dims)))
-    i || filter!(x->x!=CartesianIndex(Tuple(fill(0,dims))),ret)
-    ret
 end
 
 function logaction(p,m)
@@ -130,93 +115,16 @@ function loadlog()
     end
 end
 
-#puzzledictionary = loadlog()
-
-
-using CircularArrays
-
-function Base.insert!(a::CircularVector, i::Integer, item)
-    insert!(a.data,linearindex(a,i),item)
-    return a
-end
-function Base.deleteat!(a::CircularVector, i::Integer)
-    deleteat!(a.data,linearindex(a,i))
-    return a
-end
-function Base.deleteat!(a::CircularVector, inds)
-    deleteat!(a.data, sort!(unique(linearindices(a,inds))))
-    a
+function splitvect(a::Vector,delim)
+    inds = vcat(0,findall(==(delim),a),length(a)+1)
+    view.(Ref(a), (:).(inds[1:end-1].+1,inds[2:end].-1))
 end
 
-linearindices(a::CircularVector,inds) = map(i -> linearindex(a,i), inds)
-linearindices(a::CircularVector,i::Integer) = linearindex(a,i)
-linearindex(a::CircularVector,i::Integer) = mod(i, eachindex(IndexLinear(), a.data))
-function standardrange(a::CircularVector,r::AbstractUnitRange{<:Integer})
-    f = mod(first(r), eachindex(IndexLinear(), a.data))
-    l = f + length(r) - 1
-    f:l
-end
-function splitrange(a::CircularVector,r::AbstractUnitRange{<:Integer})
-    first(r) > last(r) && return [r]
-    f = mod(first(r), eachindex(IndexLinear(), a.data))
-    if length(r) > length(a.data)
-        l = mod(first(r)-1, eachindex(IndexLinear(), a.data))
-    else
-        l = mod(last(r), eachindex(IndexLinear(), a.data))
-    end
-    if f > l
-        [f:length(a.data), 1:l]
-    else
-        [f:l]
-    end
-end
-function shift!(a::CircularVector, i::Integer, steps)
-    item = a[i]
-    ind = mod(i, eachindex(IndexLinear(), a.data))
-    deleteat!(a.data,ind)
-    newind = linearindex(a,i+steps)
-    insert!(a.data,newind,item)
-    newind
-end
+freqdict(str) = Dict([i => count(x->x==i,str) for i in str])
 
-function shiftmove!(a::CircularVector, i::Integer, steps)
-    #this is slower than shiftdel for some reason
-    steps = mod(steps,1:length(a)-1)
-    item = a[i]
-    a[i:i+steps-1] = a[i+1:i+steps]
-    a[i+steps] = item
-    a
-end
-
-function shift!(a::CircularVector, inds, steps)
-    items = a[inds]
-    ind = sort!(unique(map(i -> mod(i, eachindex(IndexLinear(), a.data)), inds)))
-    deleteat!(a.data,ind)
-    newind = mod(i+steps, eachindex(IndexLinear(), a.data))
-    insert!(a.data,newind,item)
-    newind
-end
-
-Base.splice!(a::CircularVector, i::Integer, ins=Base._default_splice) = splice!(a.data,linearindex(a,i),ins)
-Base.splice!(a::CircularVector, inds) = (dltds = eltype(a.data)[]; _deleteat!(a.data, sort!(unique(linearindices(inds,i))), dltds); dltds)
-
-function Base.splice!(a::CircularVector, r::AbstractUnitRange{<:Integer}, ins=Base._default_splice)
-    v = a[r]
-    m = length(ins)
-    if m == 0
-        deleteat!(a, r)
-        return v
-    end
-
-    rn = splitrange(a,r)
-
-    if length(rn) == 2
-        inslen = min(length(rn[1]),length(ins))
-        v = splice!(a.data,rn[1],ins[1:inslen])
-        w = splice!(a.data,rn[2],ins[inslen+1:end])
-        return vcat(v,w)
-    else
-        return splice!(a.data,r,ins)
-    end
-
+neighbours(ci) = Ref(ci) .+ cartesiancube(length(ci))
+function cartesiancube(dims,i=false)
+    ret = CartesianIndices(Tuple(fill(-1:1,dims)))
+    i || filter!(x->x!=CartesianIndex(Tuple(fill(0,dims))),ret)
+    ret
 end
