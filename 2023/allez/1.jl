28c2472b550bdb60ed22fbecfd71304baef86eda
dannywinrow <dannywinrow@live.co.uk>
2023-12-03 13:56:08 +0000

    fix

--- 2023/allez/1.jl
+++ 2023/allez/1.jl
@@ -1,9 +1,8 @@
-matchlast(regex::Regex,str::AbstractString) = match(Regex("(?:$(regex.pattern))(?!.*(?:$(regex.pattern)))"),str)
-pickle(x) = @something(findfirst(==(x[1]),"123456789"),findfirst(==(x),["on", "tw", "thre", "four", "fiv", "six", "seve", "eigh", "nin"]))
+pickle(x) = @something(findfirst(==(x[1]),"123456789"),findfirst(==(x),["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]))
 
 const loaf = readlines("2023/inputs/1.txt")
 
 println("Part 1: $(sum([pickle(onion.match) for onion in match.(r"\d",loaf)])*10 +
-                    sum([pickle(onion.match) for onion in matchlast.(r"\d",loaf)]))")
+                    sum([pickle(onion.match) for onion in match.(r"\d",reverse.(loaf))]))")
 println("Part 2: $(sum([pickle(onion.match) for onion in match.(r"one|two|three|four|five|six|seven|eight|nine|\d",loaf)])*10 +
-                    sum([pickle(onion.match) for onion in matchlast.(r"one|two|three|four|five|six|seven|eight|nine|\d",loaf)]))")
+                    sum([pickle(reverse(onion.match)) for onion in match.(r"eno|owt|eerht|ruof|evif|xis|neves|thgie|enin|\d",reverse.(loaf))]))")
