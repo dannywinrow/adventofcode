f1efb31b862402823386040966c6d3e7df45f5bb
dannywinrow <dannywinrow@live.co.uk>
2023-12-14 12:11:24 +0000

    day 14

---
+++ 2023/src/14.jl
@@ -0,0 +1,80 @@
+include("../../helper.jl")
+
+example = """O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#...."""
+
+
+function f(row)
+    j = length(row)
+    cnt = 0
+    for (i,c) in enumerate(row)
+        if c == 'O'
+            cnt += j
+            j -= 1
+        elseif c == '#'
+            j = length(row) - i
+        end
+    end
+    cnt
+end
+
+function tiltcol!(col)
+    j = 1
+    for (i,c) in enumerate(col)
+        if c == 'O'
+            col[i] = '.'
+            col[j] = 'O'
+            j += 1
+        elseif c == '#'
+            j = i+1
+        end
+    end
+    col
+end
+tilt!(grid) = hcat(tiltcol!.(eachcol(grid))...)
+
+function cycle!(grid)
+    for _ in 1:4
+        grid = rotr90(tilt!(grid))
+    end
+    grid
+end
+
+function value(grid)
+    sum(size(grid,1) - x[1] + 1 for x in findall(==('O'),grid))
+end
+
+function solve(grid,n)
+    grids = []
+    i = 1
+    while i < n
+        grid=cycle!(copy(grid))
+        x = findfirst(==(grid),grids)
+        if isnothing(x)
+            push!(grids,grid)
+        else
+            len = length(grids) - x + 1
+            bef = x-1
+            r = (n - bef) % len
+            return value(grids[r + bef])
+        end
+        i += 1
+    end
+end
+
+n = 1000000000
+#grid = loadgrid(Char,split(example,"\n"))
+grid = loadgrid(Char)
+pt1 = value(tilt!(copy(grid)))
+pt2 = solve(grid,n)
+
+println("Part 1: $pt1")
+println("Part 2: $pt2")
